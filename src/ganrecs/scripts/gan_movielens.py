cc176c175bbe2b933742ff98f8a9776f52e90bbc
Austin Graham <austingraham731@gmail.com>
2018-07-30 00:06:04 -0500

    for port

--- src/ganrecs/scripts/gan_movielens.py
+++ src/ganrecs/scripts/gan_movielens.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import os
+import shutil
 import argparse
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 from random import randint
 
@@ -18,7 +20,7 @@ USER_COUNT = 6040
 TEST_PERCENT = .2
 
 def sample_Z(m, n):
-    return np.random.uniform(-1., 1., size=[m, n])
+    return np.random.normal(-1., 1., size=[m, n])
 
 
 def process_args(args=None):
@@ -36,6 +38,12 @@ def process_args(args=None):
 
     if not os.path.exists(location):
         os.makedirs(location)
+    else:
+        ans = input("Overwrite output directory?: ").upper()
+        if ans == 'N' or ans == 'NO':
+            print('Exiting...')
+            exit()
+        shutil.rmtree(location)
 
     return location, int(args.noise), int(args.epochs)
 
@@ -73,6 +81,15 @@ def get_sample(data, size):
     return np.array(result)
 
 
+def plot_losses(epochs, d_losses, g_losses):
+    xs = [x for x in range(epochs)]
+    plt.title('D/G Losses Over Time')
+    plt.plot(xs, d_losses, label='Discriminator')
+    plt.plot(xs, g_losses, label='Generator')
+    plt.legend()
+    plt.show()
+
+
 def main(args=None):
     location, noise, epochs = process_args(args)
     model_path = os.path.join(location, "model.ckpt")
@@ -80,10 +97,13 @@ def main(args=None):
 
     print("Constructing network...")
     dis_arch = [MOVIES_COUNT, 2000, 1000, 1]
-    gen_arch = [noise, 3000, 2000, MOVIES_COUNT]
+    gen_arch = [noise, 3000, 2000, 3000, MOVIES_COUNT]
     network = gan(dis_arch, gen_arch, MOVIES_COUNT)
 
     saver = tf.train.Saver()
+
+    d_losses = []
+    g_losses = []
 
     session = tf.Session()
     if os.path.exists(model_path + ".meta"):
@@ -100,13 +120,16 @@ def main(args=None):
             _, G_loss_curr = session.run([network.generator_optimizer, network.generator_loss], feed_dict={network.generator_input: _sample, network.generator_condition: users})
 
             if it % 100 == 0:
+                d_losses.append(D_loss_curr)
+                g_losses.append(G_loss_curr)
                 print('Iter: {}'.format(it))
-                print('D loss: {:.4}'. format(D_loss_curr))
+                print('D loss: {:.4}'.format(D_loss_curr))
                 print('G_loss: {:.4}'.format(G_loss_curr))
                 print()
         
         print("Saving model to {}".format(location))
         saver.save(session, model_path)
+        plot_losses(int(epochs / 100), d_losses, g_losses)
 
 
 if __name__ == '__main__':
--- src/ganrecs/scripts/surprise_recs.py
+++ src/ganrecs/scripts/surprise_recs.py
@@ -8,6 +8,7 @@ from surprise import Dataset
 
 from surprise.model_selection import cross_validate
 
+from surprise.prediction_algorithms.knns import KNNWithMeans
 
 def process_args(args=None):
     parser = argparse.ArgumentParser(description="Run and output statistics on SVD recommendations with MovieLens")
@@ -23,13 +24,25 @@ def main(args=None):
     out_path = os.path.expanduser(location)
     print('Checking output directory...')
     if not os.path.exists(out_path):
-        os.path.makedirs(out_path)
+        os.makedirs(out_path)
+    else:
+        ans = input("Overwrite output directory?: ").upper()
+        if ans == 'N' or ans == 'NO':
+            print('Exiting...')
+            exit()
     print("Loading dataset...")
-    data = Dataset.load_builtin('ml-1m')
+    data = Dataset.load_builtin('ml-100k')
     algo = SVD()
     print("Running SVD...")
-    sys.stdout = open(os.path.join(location, "out.txt"), 'w')
+    sys.stdout = open(os.path.join(location, "svd_out.txt"), 'w')
+    result = cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+    from pdb import set_trace; set_trace()
+    sys.stdout = sys.__stdout__
+    print("Running KNN...")
+    algo = KNNWithMeans()
+    sys.stdout = open(os.path.join(location, "knn_out.txt"), 'w')
     cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+    print("Done.")
 
 
 if __name__ == '__main__':
