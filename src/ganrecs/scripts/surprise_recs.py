56c07bfb62852e13393d407b8d3ba59eec9ea85c
Austin Graham <austin.graham@nextthought.com>
2018-07-30 11:54:36 -0500

    surprise exp gives output

--- src/ganrecs/scripts/surprise_recs.py
+++ src/ganrecs/scripts/surprise_recs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import sys
+import json
 import argparse
 
 from surprise import SVD
@@ -17,6 +18,10 @@ def process_args(args=None):
 
     return args.location
 
+def write_results_to_file(rmse, mae, title):
+    avg_rmse = sum(rmse) / len(rmse)
+    avg_mae = sum(mae) / len(mae)
+    json.dump({'rmse': avg_rmse, 'mae': avg_mae}, open(title, 'w'))
 
 def main(args=None):
     location = process_args(args)
@@ -31,17 +36,15 @@ def main(args=None):
             print('Exiting...')
             exit()
     print("Loading dataset...")
-    data = Dataset.load_builtin('ml-100k')
+    data = Dataset.load_builtin('ml-1m')
     algo = SVD()
     print("Running SVD...")
-    sys.stdout = open(os.path.join(location, "svd_out.txt"), 'w')
-    result = cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
-    from pdb import set_trace; set_trace()
-    sys.stdout = sys.__stdout__
+    result = cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=10, verbose=True)
+    write_results_to_file(result['test_rmse'], result['test_mae'], 'svd_out.json')
     print("Running KNN...")
     algo = KNNWithMeans()
-    sys.stdout = open(os.path.join(location, "knn_out.txt"), 'w')
-    cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+    result = cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=10, verbose=True)
+    write_results_to_file(result['test_rmse'], result['test_mae'], 'knn_out.json')
     print("Done.")
 
 
