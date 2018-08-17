35280d04faaabf0065be304822f39d5c7488a367
Austin Graham <austingraham731@gmail.com>
2018-08-16 22:01:42 -0500

    ml100k exp

--- src/ganrecs/scripts/gan_movielens.py
+++ src/ganrecs/scripts/gan_movielens.py
@@ -4,8 +4,8 @@ import json
 import shutil
 import argparse
 import numpy as np
-import tensorflow as tf
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 from random import randint
 from random import sample
@@ -13,10 +13,9 @@ from random import sample
 from ganrecs.data import RatingCollection
 
 from ganrecs.network import gan
+from ganrecs.network import dragan
 
 from surprise import Dataset
-
-import matplotlib.pyplot as plt
 
 old_v = tf.logging.get_verbosity()
 tf.logging.set_verbosity(tf.logging.ERROR)
@@ -24,6 +23,7 @@ tf.logging.set_verbosity(tf.logging.ERROR)
 MOVIES_COUNT = 1682
 USER_COUNT = 6040
 TEST_PERCENT = .2
+DEFAULT_EPOCHS = 50000
 
 
 def sample_Z(m, n):
@@ -32,27 +32,16 @@ def sample_Z(m, n):
 
 def process_args(args=None):
     parser = argparse.ArgumentParser(description="Test with MNIST data set")
-    parser.add_argument('-l', '--location', help='Saved model location')
     parser.add_argument('-n', '--noise', help="Amount of noise to include in network")
     parser.add_argument('-e', '--epochs', help="Number of epochs to run")
+    parser.add_argument('-f', '--file', help="Output file name")
     args = parser.parse_args(args)
 
-    assert args.location, "Must provide output location"
+    assert args.file, "Must provide output file"
     assert args.noise, "Must provide noise"
-    assert args.epochs, "Must provide epochs"
+    epochs = DEFAULT_EPOCHS if args.epochs is None else int(args.epochs)
 
-    location = os.path.expanduser(args.location)
-
-    if not os.path.exists(location):
-        os.makedirs(location)
-    else:
-        ans = input("Overwrite output directory?: ").upper()
-        if ans == 'N' or ans == 'NO':
-            print('Exiting...')
-            exit()
-        shutil.rmtree(location)
-
-    return location, int(args.noise), int(args.epochs)
+    return args.file, int(args.noise), epochs
 
 
 def get_sample(data, size):
@@ -63,29 +52,19 @@ def get_sample(data, size):
         result.append(list(data[str(i)].values()))
     return np.array(result)
 
-
-def plot_losses(epochs, d_losses, g_losses):
-    xs = [x for x in range(epochs)]
-    plt.title('D/G Losses Over Time')
-    plt.plot(xs, d_losses, label='Discriminator')
-    plt.plot(xs, g_losses, label='Generator')
-    plt.legend()
-    plt.show()
-
 def get_perturbed_batch(minibatch):
     return minibatch + 0.5 * minibatch.std() * np.random.random(minibatch.shape)
 
-def write_output(d_losses, g_losses, distances):
+def write_output(d_losses, g_losses, distances, _file):
     d_losses = [float(d) for d in d_losses]
     g_losses = [float(g) for g in g_losses]
     distances = [float(d) for d in distances]
     _dict = {'d_losses': d_losses, 'g_losses': g_losses, 'distances': distances}
-    json.dump(_dict, open('out/out50000epochsnosvd.json', 'w'))
+    json.dump(_dict, open('permout/{}'.format(_file), 'w'))
 
 
 def main(args=None):
-    location, noise, epochs = process_args(args)
-    model_path = os.path.join(location, "model.ckpt")
+    _file, noise, epochs = process_args(args)
     data = Dataset.load_builtin('ml-100k')
     rc = RatingCollection(data.raw_ratings)
 
@@ -94,55 +73,55 @@ def main(args=None):
     d_losses = []
     g_losses = []
     distances = []
-    if os.path.exists(model_path + ".meta"):
-        print("Restoring model....")
-        saver.restore(session, model_path)
-    else:
-        print("Starting run...")
-        distances = []
-        for i in range(len(rc.folds)):
-            training_data = {}
-            for idx, value in enumerate(rc.folds):
-                #if idx != i:
-                training_data = {**training_data, **rc._get_matrix(value)}
-            dis_arch = [MOVIES_COUNT, 300, 1]
-            gen_arch = [noise, 300, MOVIES_COUNT]
-            tf.reset_default_graph()
-            network = gan(dis_arch, gen_arch, MOVIES_COUNT, 50)
-            session = tf.Session()
-            session.run(tf.global_variables_initializer())
-            for it in range(epochs):
-                users = get_sample(training_data, 50)
-                _sample = sample_Z(50, noise)
-                users_p = get_perturbed_batch(users)
-                _, D_loss_curr = session.run([network.discriminator_optimizer, network.discriminator_loss],
-                feed_dict={network.discriminator_input: users, network.generator_input: _sample,
-                network.generator_condition: users, network.pert: users_p})
-                _, G_loss_curr = session.run([network.generator_optimizer, network.generator_loss],
-                feed_dict={network.generator_input: _sample, network.generator_condition: users})
+    print("Starting run...")
+    distances = []
+    for i in range(len(rc.folds)):
+        training_data = {}
+        for idx, value in enumerate(rc.folds):
+            #if idx != i:
+            training_data = {**training_data, **rc._get_matrix(value)}
+        dis_arch = [MOVIES_COUNT, 300, 1]
+        gen_arch = [noise, 300, MOVIES_COUNT]
+        tf.reset_default_graph()
+        network = dragan(dis_arch, gen_arch, MOVIES_COUNT, 50)
+        session = tf.Session()
+        session.run(tf.global_variables_initializer())
+        for it in range(epochs):
+            users = get_sample(training_data, 50)
+            _sample = sample_Z(50, noise)
+            users_p = get_perturbed_batch(users)
+            _, D_loss_curr = session.run([network.discriminator_optimizer, network.discriminator_loss],
+            feed_dict={network.discriminator_input: users, network.generator_input: _sample,
+            network.generator_condition: users, network.pert: users_p, network.keep_prob: 0.5})
+            # _, D_loss_curr = session.run([network.discriminator_optimizer, network.discriminator_loss],
+            # feed_dict={network.discriminator_input: users, network.generator_input: _sample,
+            # network.generator_condition: users})
+            _, G_loss_curr = session.run([network.generator_optimizer, network.generator_loss],
+            feed_dict={network.generator_input: _sample, network.generator_condition: users})
 
-                if it % 100 == 0:
-                    d_losses.append(D_loss_curr)
-                    g_losses.append(G_loss_curr)
-                    print('D loss: {:.4}'.format(D_loss_curr))
-                    print('G_loss: {:.4}'.format(G_loss_curr))
+            if it % 100 == 0:
+                d_losses.append(D_loss_curr)
+                g_losses.append(G_loss_curr)
+                print('Iteration {} of {} ---------------'.format(it, epochs))
+                print('D loss: {:.4}'.format(D_loss_curr))
+                print('G_loss: {:.4}'.format(G_loss_curr))
 
-            # Get the classification distances
-                    test_fold = rc._get_matrix(rc.folds[i])
-                    sample_size = len(test_fold)
-                    users = get_sample(test_fold, sample_size).astype(np.float32)
-                    _sample = sample_Z(sample_size, noise)
-                    generated_images = session.run(network.generator.prob, feed_dict={network.generator_input: _sample,             
-                    network.generator_condition: users})
+                # Get the classification distances
+                test_fold = rc._get_matrix(rc.folds[i])
+                sample_size = len(test_fold)
+                users = get_sample(test_fold, sample_size).astype(np.float32)
+                _sample = sample_Z(sample_size, noise)
+                generated_images = session.run(network.generator.prob, feed_dict={network.generator_input: _sample,             
+                network.generator_condition: users})
 
-                    feed_users = get_sample(test_fold, sample_size).astype(np.float32)
-                    feed_users = tf.convert_to_tensor(feed_users, dtype=tf.float32)
-                    generated_images = tf.convert_to_tensor(generated_images, dtype=tf.float32)
-                    result = tf.contrib.gan.eval.frechet_classifier_distance_from_activations(feed_users, generated_images)
-                    result = session.run(result)
-                    distances.append(result)
-            write_output(d_losses, g_losses, distances)
-            break
+                feed_users = get_sample(test_fold, sample_size).astype(np.float32)
+                feed_users = tf.convert_to_tensor(feed_users, dtype=tf.float32)
+                generated_images = tf.convert_to_tensor(generated_images, dtype=tf.float32)
+                result = tf.contrib.gan.eval.frechet_classifier_distance_from_activations(feed_users, generated_images)
+                result = session.run(result)
+                distances.append(result)
+        write_output(d_losses, g_losses, distances, _file)
+        break
             
 
 
